c08aa4b95e0b5a402f1ddfb9e5aef1385b994d58
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2017-06-21 16:44:55 +0300

    public inheritance with pure virtual function. Vectors cannot contain those objects, so switched to base class handles. Class names shown with typeid. Still leaks memory...

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <typeinfo>
 
 class Exercise
 {
 public:
+    Exercise()
+    {
+        std::cout << "Exercise constructor " << this << std::endl;
+    }
+    virtual ~Exercise(){};
+    virtual void execute() = 0;
+    void log() const
+    {
+        std::cout << typeid(*this).name() << " executed " << this << std::endl;
+    }
+};
+
+class Something : public Exercise
+{
     void execute()
     {
-        std::cout << "executed" << std::endl;
+        log();
+    }
+};
+
+class Otherthing : public Exercise
+{
+    void execute()
+    {
+        log();
     }
 };
 
 int main(void)
 {
-    std::vector<Exercise> exercises;
-    exercises.push_back(*(new Exercise()));
-    for (std::vector<Exercise>::iterator it = exercises.begin(); it != exercises.end(); ++it)
+    std::vector<Exercise*> exercises;
+    exercises.push_back(new Something());
+    exercises.push_back(new Otherthing());
+    for (std::vector<Exercise*>::iterator it = exercises.begin(); it != exercises.end(); ++it)
     {
-        it->execute();
+        (*it)->execute();
+        std::cout << typeid(it).name() << std::endl;
+        std::cout << typeid(*it).name() << std::endl;
+        std::cout << typeid(*(*it)).name() << std::endl;
     }
     return 0;
 }
